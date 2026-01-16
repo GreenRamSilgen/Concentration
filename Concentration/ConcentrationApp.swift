ea014a454bc536ad1612a46e1813d073fd8040e5
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-16 08:27:30 -0800

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Kiran Shrestha on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
